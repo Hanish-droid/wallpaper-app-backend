c517d3cf0c7a15c79749476cc0b1cf7e9185dd30
MdHanish <mdhanish132@gmail.com>
2025-04-28 14:49:33 +0530

    commiting changes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 .catch(err => console.error('MongoDB connection error:', err));
 
 // Listen on dynamic PORT
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
